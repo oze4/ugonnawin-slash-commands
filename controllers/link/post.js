da644230dfac1b6cee9322553cf121b6b1221e69
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-16 14:51:07 -0500

    Update post.js

--- controllers/link/post.js
+++ controllers/link/post.js
@@ -9,7 +9,12 @@ const router  = express.Router();
 
 
 router.post('/new', (req, res) => {
-    console.log(req);
+    console.log("------------ req.body ---------------");
+    console.log(req.body);
+    console.log("----------- end req.body ------------\r\n\r\n");
+    console.log("------------ req.params ---------------");
+    console.log(req.params);
+    console.log("----------- end req.params ------------\r\n\r\n");
     res.status(200).send('10-4');
 });
 
