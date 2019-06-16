a7dd5ef73e19dc1d5ef84f9a6980a4f4f471a53c
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-16 17:29:12 -0500

    updates: got verification middleware working; removed logging; cleaned up docker-compose documentation

--- controllers/link/post.js
+++ controllers/link/post.js
@@ -13,13 +13,7 @@ const middleware = require('../../utils/middleware.js');
 router.use(middleware.request.verifySlackRequest);
 
 router.post('/new', (req, res) => {
-    console.log("------------ req.body ---------------");
-    console.log(req.body);
-    console.log("----------- end req.body ------------\r\n\r\n");
-    console.log("------------ req.headers ---------------");
-    console.log(req.headers);
-    console.log("----------- end req.headers ------------\r\n\r\n");
-    res.status(200).send('10-4');
+    res.status(200).send(req.body.text);
 });
 
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -15,36 +15,10 @@ const middleware = {
 
     request: {
         verifySlackRequest(req, res, next) {
-            let slackSignature = req.headers['x-slack-signature'];
-            if (!slackSignature) {
-                return res.status(200).send('no slack signature');
+            if (_validateSlackRequest(config.slack.signingSecret, config.slack.versionNumber, req, res)) {
+                next()
             } else {
-                let timeStamp = req.headers['x-slack-request-timestamp'];
-                if (_lessThanFiveMinutes(timeStamp)) {
-                    if (_validateSlackRequest(config.slack.signingSecret, req, res, true)) {
-                        next()
-                    } else {
-                        res.status(200).send("Slack signature does not match hash!");
-                    }
-                    /*let bodyString = qs.stringify(req.body, { format: 'RFC1738' });  // If RFC1738 is not used it will fail
-                    let bs = JSON.stringify(req.body);
-                    let reqSignature = `${config.slack.versionNumber}:${timeStamp}:${bodyString}`;
-                    let hashed = crypto.createHmac('sha256', config.slack.signingSecret).update(reqSignature, 'utf8').digest('hex');
-                    console.log("signingSecret: " + config.slack.signingSecret);
-                    console.log("qs.stringify: " + bodyString);
-                    console.log("JSON.stringify: " + req.body());
-                    console.log("hashedSignature: " + hashed);
-                    let sig = `${config.slack.versionNumber}=${hashed}`;
-                    if (crypto.timingSafeEqual(Buffer.from(sig, 'utf8'), Buffer.from(slackSignature, 'utf8'))) {
-                        next();
-                    } else {
-                        return res.status(200).send('Verification failed');
-                    }*/
-                } else {
-                    // If the request is older than 5 minutes, it is possible it may be a replay attack
-                    // so we drop it.
-                    return res.status(200).send('older than five min');
-                }
+                res.status(400).send("Slack signature does not match hash!");
             }
         }
     }
@@ -52,39 +26,41 @@ const middleware = {
 
 
 function _lessThanFiveMinutes(reqTimestamp) {
-    let now = new Date(),
-        then = new Date(Number(reqTimestamp)),
-        FIVE_MIN = 5 * 60 * 1000;
-    console.log(typeof(then))
-    return (then - now) < FIVE_MIN ? true : false;
+    let FIVE_MIN = 5 * 60 * 1000;
+    return ((new Date(Number(reqTimestamp))) - (new Date())) < FIVE_MIN;
 }
 
-function _validateSlackRequest (slackAppSigningSecret, httpReq, httpRes, logging) {
-    // from:
-    // https://github.com/gverni/validate-slack-request/blob/master/index.js
-    logging = logging || false
-    if (typeof logging !== 'boolean') {
-      throw new Error('Invalid type for logging. Provided ' + typeof logging + ', expected boolean')
-    }
+function _validateSlackRequest (slackAppSigningSecret, slackVersionNumber, httpReq, httpRes) {
+    /**
+     * !!~ REQUIRES 'querystring' PACKAGE ~!!
+     * 
+     * from: https://github.com/gverni/validate-slack-request/blob/master/index.js
+     */
+
     if (!slackAppSigningSecret || typeof slackAppSigningSecret !== 'string' || slackAppSigningSecret === '') {
-      return httpRes.status(200).send('Invalid slack app signing secret')
+        return httpRes.status(400).send('Slack signing secret empty or not a string');
     }
-    const xSlackRequestTimeStamp = httpReq.get('X-Slack-Request-Timestamp')
+
     const SlackSignature = httpReq.get('X-Slack-Signature')
-    const bodyPayload = qs.stringify(httpReq.body).replace(/%20/g, '+') // Fix for #1
+    if (!SlackSignature) {
+        return res.status(400).send('No Slack signature found in request');
+    }
+
+    const xSlackRequestTimeStamp = httpReq.get('X-Slack-Request-Timestamp')
+    if (!_lessThanFiveMinutes(xSlackRequestTimeStamp)) {
+        // If the request is older than 5 minutes, it is possible it may be a replay attack so we drop it.
+        return res.status(400).send('older than five min');
+    }
+
+    const bodyPayload = qs.stringify(httpReq.body).replace(/%20/g, '+');
     if (!(xSlackRequestTimeStamp && SlackSignature && bodyPayload)) {
-      if (logging) { console.log('Missing part in Slack\'s request') }
-      //return false
-      return httpRes.status(200).send('Invalid Slack Request');
-    }
-    const baseString = 'v0:' + xSlackRequestTimeStamp + ':' + bodyPayload
-    const hash = 'v0=' + crypto.createHmac('sha256', slackAppSigningSecret).update(baseString).digest('hex')
-
-    if (logging) {
-      console.log('Slack verifcation:\n Request body: ' + bodyPayload + '\n Calculated Hash: ' + hash + '\n Slack-Signature: ' + SlackSignature)
+      return httpRes.status(400).send('Invalid request from Slack');
     }
 
-    return (SlackSignature === hash)
+    const baseString = `${slackVersionNumber}:${xSlackRequestTimeStamp}:${bodyPayload}`;
+    const hash = `${slackVersionNumber}=${crypto.createHmac('sha256', slackAppSigningSecret).update(baseString).digest('hex')}`;
+
+    return (SlackSignature === hash);
   }
 
 
