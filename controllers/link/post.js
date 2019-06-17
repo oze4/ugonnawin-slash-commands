a937186ccb037dce4883379b619f87a3984482c5
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-16 20:55:23 -0500

    update: cleaned up helper; added more comments; misc cleanup and updates

--- controllers/link/post.js
+++ controllers/link/post.js
@@ -1,94 +1,86 @@
-// 
 // Routes for POST /link/__
-//
 
 'use strict'
 const express     = require('express');
 const router      = express.Router();
 const middleware  = require('../../utils/middleware.js');
-const resHelper   = require('../../utils/helper.js');
+const helper      = require('../../utils/helper.js');
 const config      = require('../../utils/config.js');
 
 
-// Middleware to verify request is from Slack.
-// Currently, only set up when POSTing to '/link' routes
+// Middleware to verify request is from Slack.  
+// Currently, only set up when POSTing to '/link' routes.
 router.use(middleware.request.verifySlackRequest);
 
 
+//================================
 // ROUTE: /link/new
+//================================
 router.post('/new', (req, res) => {
+
+    // Verify we have a URL (this checks if we have periods and at least two characters at the end after 
+    //     a period: eg: someWordWithAt.LeastTwoCharsAtE.nd). That would qualify as a URL. We verify the
+    //     string has either 'http' or 'https' below, if it doesn't we add 'http' (and let the website)
+    //     handle redirection to 'https', if it does. Basically we play it safe.
     const looselyDefinedUrlRegex = RegExp('[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+');
     if (looselyDefinedUrlRegex.test(req.body.text)) {
-        // End current request so we can send interactive request
+
+        // Since we are sending our own POST (have to because we want interactive message)
+        //     we need to end the current request. This is best practices.. Notice how we
+        //     are not sending a response, we are just ending it - this will prevent slack
+        //     from erroring out, and will wait for our response until timeout is reached.
         res.status(200).end();
-        // Send interactive request..
+
         let responseUrl = req.body.response_url;
         if (req.body.token != config.slack.verificationToken) {
+
+            // Verify tokens match before we respond.
             res.status(403).send("Access denied");
+
         } else {
+
+            //TODO: turn this into a helper method
+            let isValidUrl = req.body.text.startsWith("http://") || req.body.text.startsWith("https://");
+            let url = isValidUrl ? req.body.text : `http://${req.body.text}`;
             let jsonMessage = {
                 "text": "Click button to open URL",
                 "attachments": [
                     {
-                        "fallback": ""+ req.body.text +"",
+                        "fallback": ""+ url +"",
                         "actions": [
                             {
-                                "text": "Open URL",
+                                "text": ""+ url +"",
                                 "type": "button",
-								"url": ""+ req.body.text +""
+								"url": ""+ url +""
                             }
                         ]
                     }
                 ]
-            };
-            var message = {
-                "text": "This is your first interactive message",
-                "attachments": [
-                    {
-                        "text": "Building buttons is easy right?",
-                        "fallback": "Shame... buttons aren't supported in this land",
-                        "callback_id": "button_tutorial",
-                        "color": "#3AA3E3",
-                        "attachment_type": "default",
-                        "actions": [
-                            {
-                                "name": "yes",
-                                "text": "yes",
-                                "type": "button",
-                                "value": "yes"
-                            },
-                            {
-                                "name": "no",
-                                "text": "no",
-                                "type": "button",
-                                "value": "no"
-                            },
-                            {
-                                "name": "maybe",
-                                "text": "maybe",
-                                "type": "button",
-                                "value": "maybe",
-                                "style": "danger"
-                            }
-                        ]
-                    }
-                ]
-            }
-            resHelper.slack.slash.response.sendMessageToSlackResponseURL(responseUrl, jsonMessage);
+            }; //END-TODO: turn this into a helper method
+
+            // Send POST response with buttons (aka interactive message - but this message
+            //     is not 'interactive' as defined by Slack).
+            helper.responses.sendMessageToSlackResponseURL(responseUrl, jsonMessage);
+
         }
-        /*
-        res.setHeader('content-type', 'application/json');
-        let responseMessage = resHelper.slack.slash.response.buttonMessage(req.body.text, 'in_channel');
-        res.status(200).send(JSON.parse(responseMessage));
-        */
+
     } else {
+
+        // If we are not provided a valid URL, get an invalid response and return it
         res.status(200).send(middleware.request.getInvalidUrlResponse());
+
     }
+
 });
 
-// ROUTE: /link/interactive
-router.post('/interactive', (req, res) => {
 
+//================================
+// ROUTE: /link/interactive 
+//================================
+router.post('/interactive', (req, res) => {
+    //TODO: complete this if we want interactive buttons
+    //      https://api.slack.com/tutorials/intro-to-message-buttons
+    res.status(400).send("Hmm.. can't find that..");
 });
 
 
--- utils/helper.js
+++ utils/helper.js
@@ -5,48 +5,22 @@ const request = require('request')
 
 
 module.exports = {
-    slack: {
-        slash: {
-            response: {
-                buttonMessage (message, responseType) {
-                    let resType = responseType !== 'in_channel' || 'ephemeral' ? 'in_channel' : responseType;
-                    let jsonMessage = `
-                    {
-                        "response_type": "${resType}",
-                        "text": "Click button to open URL",
-                        "attachments": [
-                            {
-                                "fallback": "${String(message)}",
-                                "actions": [
-                                    {
-                                        "type": "button",
-                                        "url": "${message}"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                    `;
-                    return JSON.stringify(JSON.parse(jsonMessage));
+    responses: {
+        sendMessageToSlackResponseURL (responseURL, JSONmessage) {
+            var postOptions = {
+                uri: responseURL,
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
                 },
-
-                sendMessageToSlackResponseURL (responseURL, JSONmessage) {
-                    var postOptions = {
-                        uri: responseURL,
-                        method: 'POST',
-                        headers: {
-                            'Content-type': 'application/json'
-                        },
-                        json: JSONmessage
-                    }
-                    request(postOptions, (error, res, body) => {
-                        if (error){
-                            // handle errors as you see fit
-                            res.status(200).send("Something went wrong! " + error);
-                        }
-                    })
-                }
+                json: JSONmessage
             }
-        }
+            request(postOptions, (error, res, body) => {
+                if (error){
+                    // handle errors as you see fit
+                    res.status(400).send("Something went wrong! " + error);
+                }
+            })
+        },
     },
 }
