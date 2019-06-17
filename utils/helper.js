ca2855dadb8366f2b181ef14bd35e3caad155d93
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-16 19:23:23 -0500

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -2,23 +2,25 @@
 'use strict'
 
 
-export function messageAsJson (message, channelId) {
-    let jsonMessage = `
-    {
-        "text": "Click button to open URL",
-        "channel": "${channelId}",
-        "attachments": [
-            {
-                "fallback": "${String(message)}",
-                "actions": [
-                    {
-                        "type": "button",
-                        "url": "${message}"
-                    }
-                ]
-            }
-        ]
+module.exports = {
+    messageAsJson (message, channelId) {
+        let jsonMessage = `
+        {
+            "text": "Click button to open URL",
+            "channel": "${channelId}",
+            "attachments": [
+                {
+                    "fallback": "${String(message)}",
+                    "actions": [
+                        {
+                            "type": "button",
+                            "url": "${message}"
+                        }
+                    ]
+                }
+            ]
+        }
+        `;
+        return JSON.stringify(JSON.parse(jsonMessage));
     }
-    `;
-    return JSON.stringify(JSON.parse(jsonMessage));
 }
