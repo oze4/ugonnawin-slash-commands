0e289fb65c619ac986ceed35a1e60d72274712ed
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-16 19:40:28 -0500

    update: fine-tune helper, add content-type header to response.. trying to get formatted messages to work

--- controllers/link/post.js
+++ controllers/link/post.js
@@ -4,10 +4,10 @@
  * 
  */
 'use strict'
-const express           = require('express');
-const router            = express.Router();
-const middleware        = require('../../utils/middleware.js');
-const { messageAsJson } = require('../../utils/helper.js');
+const express     = require('express');
+const router      = express.Router();
+const middleware  = require('../../utils/middleware.js');
+const resHelper   = require('../../utils/helper.js');
 
 
 // Middleware to verify request is from Slack.
@@ -19,7 +19,8 @@ router.use(middleware.request.verifySlackRequest);
 router.post('/new', (req, res) => {
     const looselyDefinedUrlRegex = RegExp('[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+');
     if (looselyDefinedUrlRegex.test(req.body.text)) {
-        let responseMessage = messageAsJson(req.body.text, req.body.channel_id)
+        res.setHeader('content-type', 'application/json');
+        let responseMessage = resHelper.slack.slash.response.buttonMessage(req.body.text, 'in_channel');
         res.status(200).send(JSON.parse(responseMessage));
     } else {
         res.status(200).send(middleware.request.getInvalidUrlResponse());
--- utils/helper.js
+++ utils/helper.js
@@ -3,24 +3,31 @@
 
 
 module.exports = {
-    messageAsJson (message, channelId) {
-        let jsonMessage = `
-        {
-            "text": "Click button to open URL",
-            "channel": "${channelId}",
-            "attachments": [
-                {
-                    "fallback": "${String(message)}",
-                    "actions": [
-                        {
-                            "type": "button",
-                            "url": "${message}"
-                        }
-                    ]
+    slack: {
+        slash: {
+            response: {
+                buttonMessage (message, responseType) {
+                    let resType = responseType !== 'in_channel' || 'ephemeral' ? 'in_channel' : responseType;
+                    let jsonMessage = `
+                    {
+                        "response_type": "${resType}",
+                        "text": "Click button to open URL",
+                        "attachments": [
+                            {
+                                "fallback": "${String(message)}",
+                                "actions": [
+                                    {
+                                        "type": "button",
+                                        "url": "${message}"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                    `;
+                    return JSON.stringify(JSON.parse(jsonMessage));
                 }
-            ]
+            }
         }
-        `;
-        return JSON.stringify(JSON.parse(jsonMessage));
-    }
+    },
 }
