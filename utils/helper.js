04568eaa51aca94f05618820e9d8557463c2bfc6
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-17 21:48:30 -0500

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -71,7 +71,7 @@ const helper = {
                 }
             };
             request(getOptions, (error, res, body) => {
-                console.log(res);
+                console.log(res.body);
                 if (error) {
                     console.log(error);
                     res.status(404).send("Something went wrong! " + error);
