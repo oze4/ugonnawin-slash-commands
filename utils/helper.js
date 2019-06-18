9017a1eebbd0338d3dd894c0711bb481012080f5
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-17 22:10:37 -0500

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -70,9 +70,8 @@ const helper = {
                 }
             };
             request(getOptions, (error, res, body) => {
-                console.log(JSON.parse(res.toJSON().body));
-                console.log("\r\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\r\n")
-                console.log(body);
+                let bbody = JSON.parse(body);
+                console.log(bbody.user.profile);
                 if (error) {
                     console.log(error);
                     res.status(404).send("Something went wrong! " + error);
