09e2dd573f1a214ad1de826c89f9a004af8717e7
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-17 21:12:45 -0500

    Update middleware.js

--- utils/middleware.js
+++ utils/middleware.js
@@ -32,17 +32,12 @@ const middleware = {
         headersAndbody (req, res, next) {
             console.log("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*");
             console.log("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*");
-            console.log();
-            console.log("USER:");
-            console.log(req.session)
-            console.log("ENDUSER");
             console.log("----------------------- req.headers ------------------------------");
             console.log(req.headers);
             console.log("--------------------- end req.headers ----------------------------");
             console.log("------------------------- req.body -------------------------------");
             console.log(req.body);
             console.log("------------------------ end req.body ----------------------------");
-            console.log();
             console.log("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*");
             console.log("~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*\r\n\r\n");
             next();
