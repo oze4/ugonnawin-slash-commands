01fceda5f014b59b9ca1947529cd0463f487c38d
oze4 <matthewpoestreich@gmail.com>
2019-06-19 10:23:21 -0500

    Update index.js

--- utils/ipapi/index.js
+++ utils/ipapi/index.js
@@ -7,7 +7,7 @@ const config  = require('../../utils/config.js');
 
 class IpApi {
     static getCoordinates (ipAddress, callback) {
-        let url = config.IpApi.baseUrl + "/" + ipAddress + "/json";
+        let url = config.ipApi.baseUrl + "/" + ipAddress + "/json";
         let getOptions = {
             uri: url,
             method: 'GET',
