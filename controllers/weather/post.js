59567a8488d673747568d4c80573262c5474b990
oze4 <matthewpoestreich@gmail.com>
2019-06-20 11:46:45 -0500

    Update post.js

--- controllers/weather/post.js
+++ controllers/weather/post.js
@@ -22,21 +22,17 @@ router.post('/', (req, res) => {
         if (err) {
             res.status(200).send("Unable to complete that action :cry: " + err);
         } else if (data) {
-            try { 
-                let weather = data;
-                console.log(data);
-                let weatherInfo = slack.messageBuilder.currentWeather(
-                        weather.name, 
-                        weatherApi.kelvinToFahrenheit(weather.main.temp), 
-                        weather.weather.description,
-                        weatherApi.getWeatherIconUrl(weather.weather.icon)
-                    );
-                console.log('weatherinfo:');
-                console.log(weatherInfo);
-                slack.api.post.jsonMessage(req.body.response_url, weatherInfo);
-            } catch {
-                slack.api.post.jsonMessage(req.body.response_url, "Uh oh!");
-            }
+            let weather = data;
+            console.log(data);
+            let weatherInfo = slack.messageBuilder.currentWeather(
+                    weather.name, 
+                    weatherApi.kelvinToFahrenheit(weather.main.temp), 
+                    weather.weather.description,
+                    weatherApi.getWeatherIconUrl(weather.weather.icon)
+                );
+            console.log('weatherinfo:');
+            console.log(weatherInfo);
+            slack.api.post.jsonMessage(req.body.response_url, weatherInfo);
         } else {
             res.status(200).send("We were unable to get weather info, and we received no errors.. Try again later :cry:");
         }
