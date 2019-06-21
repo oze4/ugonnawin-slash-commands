751671c755db09d9383b2997173d7d4ae034ef4e
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-20 20:40:00 -0500

    Update post.js

--- controllers/weather/post.js
+++ controllers/weather/post.js
@@ -19,7 +19,7 @@ router.post('/', (req, res) => {
     res.status(200).end(); // Have to send 200 within 3000ms
     let query = req.body.text.trim();
     if (query === "-h") {
-        let helpText = "You may search by city name or by ZIP code. \nIf the city or ZIP code provide incorrect data, you may need to specify a *2 character* country code!!\nThe search parameters ARE NOT case sensitive.\nSome examples are below:\n  • /weather 77065,US *(comma without a space is required!!)*\n  • /weather Houston\n  • /weather Houston,US"
+        let helpText = "You may search by city name or by ZIP code. \nIf the city or ZIP code provide incorrect data, you may need to specify a *2 character* country code!!\nThe search parameters ARE NOT case sensitive.\nSome examples are below:\n  • /weather 77065,US *(comma without a space is required!!)*\n  • /weather Houston\n  • /weather 77065\n  • /weather Houston,US"
         slack.api.post.jsonMessage(req.body.response_url, {text: helpText});
     } else { 
         weatherApi.getCurrentWeather(query, (data, err) => {
