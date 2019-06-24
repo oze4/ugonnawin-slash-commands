092bd9cd35c2d51ba01223ce3b39ff0e10b57e80
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-23 20:19:15 -0500

    Update post.js

--- controllers/interactive/post.js
+++ controllers/interactive/post.js
@@ -5,6 +5,7 @@ const express     = require('express');
 const router      = express.Router();
 const middleware  = require('../../utils/middleware.js');
 const slack       = require('../../utils/slack');
+const weatherApi  = require('../../utils/weatherapi');
 
 
 // Middleware to verify request is from Slack.
@@ -38,9 +39,33 @@ router.post('/', (req, res) => {
         let select = slack.messageBuilder.textWithSelect("Please select an option:", "Locations", myOptions);
         slack.api.post.jsonMessage(req.body.response_url, select);
     } else if (req.body.payload) {
+        let payLoad = JSON.parse(req.body.payload);
+        let userQuery = payLoad.actions.selected_option.value;
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-        console.log(JSON.parse(req.body.payload).actions.selected_option.value);
+        console.log(userQuery);
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");        
+        console.log(payLoad.response_url);
+        weatherApi.getCurrentWeather(userQuery, false, (data, err) => {
+            if (err) {
+                res.status(200).send("Unable to complete that action :cry: " + err);
+            } else if (data) {
+                try { 
+                    let weather = JSON.parse(data);
+                    let mainWeather = weather.weather[0];            
+                    let weatherInfo = slack.messageBuilder.currentWeather(
+                        weather.name, // City name
+                        weatherApi.kelvinToFahrenheit(weather.main.temp), // Temp in Fahrenheit
+                        mainWeather.description, // Short description of weather
+                        weatherApi.getWeatherIconUrl(mainWeather.icon), // Icon representing weather conditions
+                    );
+                    slack.api.post.jsonMessage(payLoad.response_url, weatherInfo);
+                } catch {
+                    slack.api.post.jsonMessage(payLoad.response_url, {text: "Unable to find weather for that location!"});
+                }
+            } else {
+                res.status(200).send("We were unable to get weather info, and we received no errors.. Try again later :cry:");
+            }
+        });
     }
 });
 
