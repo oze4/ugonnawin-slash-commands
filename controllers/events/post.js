7ec32f53e83d6d3a8129452e249c374ca7a88da9
Matt Oestreich <matthewpoestreich@gmail.com>
2019-10-17 18:49:53 -0500

    Update post.js

--- controllers/events/post.js
+++ controllers/events/post.js
@@ -16,24 +16,14 @@ const middleware = require('../../utils/middleware.js');
 // Middleware to verify request is from Slack.  
 //router.use(middleware.request.verifySlackRequest);
 
-const SALUTATIONS = [
-    "Hello",
-    "Whats the deal",
-    "What up",
-    "How's it going",
-    "Say",
-    "Whats crack-a-lackin",
-    "Pull my finger",
-    "WAAAAAZZZZUPPPP"
-];
 
-function getRandomWelcome(wordList) {
-    return wordList[Math.random() * wordList.length]
+function getRandomElementFromArray(arr) {
+    return arr[Math.random() * arr.length]
 }
 
-async function botResponse(jsonResponse, channel) {
+function botResponse(jsonResponse, channel) {
     try {
-        await fetch("https://slack.com/api/chat.postMessage", {
+        fetch("https://slack.com/api/chat.postMessage", {
             method: 'POST',
             headers: {
                 'Content-type': "application/json",
@@ -55,7 +45,11 @@ async function getRandomSubredditPost(subreddit) {
 
 function getPicFromSubreddit(subreddit, callback) {
     getRandomSubredditPost(subreddit)
-        .then(r => callback(r.data.url))
+        .then(r => {
+            r.data.url.endsWith('jpg')
+                ? callback(r.data.url)
+                : getPicFromSubreddit(subreddit, callback)
+        })
         .catch(e => { throw e });
 }
 
@@ -68,6 +62,40 @@ function makeSlackImageAttachment(url, fallback = null) {
     }
 }
 
+function isTittyEvent(text, id = '<@UPKCHH806>') {
+    return [
+        `${id} tiddies`,
+        `${id} tits`,
+        `${id} boobs`,
+        `${id} titties`,
+    ].includes(text);
+}
+
+function isCatEvent(text, id = '<@UPKCHH806>') {
+    return [
+        `${id} cat`,
+        `${id} kittie`,
+        `${id} cats`,
+        `${id} kitten`,
+    ].includes(text);
+}
+
+const salutations = [
+    "Hello",
+    "Whats the deal",
+    "What up",
+    "How's it going",
+    "Say",
+    "Whats crack-a-lackin",
+    "Pull my finger",
+    "WAAAAAZZZZUPPPP"
+];
+
+const types = {
+    APP_MENTION: "app_mention",
+    MESSAGE: "message"
+}
+
 
 /**
  * @route /events
@@ -77,29 +105,30 @@ router.post('/', (req, res, next) => {
     res.status(200).end(); // Have to send 200 within 3000ms
 
     const constants = {
-        CHANNEL: req.body.event.channel
+        CHANNEL: req.body.event.channel,
+        USER: req.body.event.user,
+        TEXT: req.body.event.text,
+        TYPE: req.body.event.type,
     }
 
-    if (req.body.event.type === "app_mention") {
-        if (req.body.event.text === "<@UPKCHH806> tiddies") {
+    if (constants.TYPE === types.APP_MENTION) {
+        if (isTittyEvent(constants.TEXT)) {
             getPicFromSubreddit('tits', tit => {
                 botResponse(makeSlackImageAttachment(tit), constants.CHANNEL)
             });
-        } else if (req.body.event.text === "<@UPKCHH806> kitties") {
+        } else if (isCatEvent(constants.TEXT)) {
             getPicFromSubreddit('cats', cat => {
                 botResponse(makeSlackImageAttachment(cat), constants.CHANNEL)
             });
         } else {
-            let message = { text: `${getRandomWelcome(SALUTATIONS)}, <@${req.body.event.user}>!!` };
+            let message = { text: `${getRandomElementFromArray(salutations)}, <@${constants.USER}>!!` };
             botResponse(message, constants.CHANNEL);
         }
     }
 
-    if (req.body.event.type === "message" && req.body.event.text === "BOBBY") {
-        botResponse({
-            text: `I don't do anything yet, but I am at your service, <@${req.body.event.user}>!!`,
-            channel: req.body.event.channel
-        });
+    if (constants.TYPE === types.MESSAGE && constants.TEXT === "BOBBY") {
+        let message = { text: `I don't do anything yet, but I am at your service, <@${constants.USER}>!!` }
+        botResponse(message, constants.CHANNEL);
     }
 
 });
