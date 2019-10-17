d051b66f0fd0dd1530cd2e211477995c671a0e4d
oze4 <matthewpoestreich@gmail.com>
2019-10-17 10:48:23 -0500

    Update post.js

--- controllers/events/post.js
+++ controllers/events/post.js
@@ -1,28 +1,43 @@
 // Routes for POST /events/__
 
 'use strict'
-const express     = require('express');
-const router      = express.Router();
-const middleware  = require('../../utils/middleware.js');
+const express = require('express');
+const router = express.Router();
+const slack = require('../../utils/slack');
+const config = require('../../utils/config');
+const request = require('request');
+const middleware = require('../../utils/middleware.js');
 
 
 // Middleware to verify request is from Slack.  
 //router.use(middleware.request.verifySlackRequest);
 
 
-// ===========================
-// Route: POST /events
-// ===========================
-// NOTE: THIS HAS TO BE HERE!!! DO NOT REMOVE THIS ROUTE!!
-// SLACK WILL SEND A CHALLENGE TO THIS ENDPOINT AND WE HAVE TO 
-// RESPOND WITH THE CHALLENGE VALUE.
 router.post('/', (req, res, next) => {
     //TODO: https://api.slack.com/docs/message-link-unfurling
-    console.log("NEW LINK_SHARED EVENT");
     console.log(req.body)
     console.log(req.headers);
-    res.status(200).send(req.body.challenge);
+    //res.status(200).send(req.body.challenge);
+    res.status(200).end(); // Have to send 200 within 3000ms
+    if (req.event.type === "app_mention") {
+        request({
+            uri: "https://slack.com/api/chat.postMessage",
+            method: 'POST',
+            headers: {
+                'Content-type': contentType,
+                'Authorization': `Bearer ${config.slack.oAuthAccessToken}`
+            },
+            json: {
+                text: `Hello <${req.body.event.user}>!!`,
+                channel: `${req.body.event.channel}`
+            }
+        }, (error, res, body) => {
+            if (error) {
+                res.status(404).send("Something went wrong! " + error);
+            }
+        })
+    }
 })
 
 
-module.exports = router; 
+module.exports = router;
